5115feb57f2b4685e7b56f8d308cb1eadbf68755
Diego <77700523+kriseren@users.noreply.github.com>
2023-06-12 23:31:26 +0200

    Update merossController.py
    Detects if the device is On or Off and does the contrary. For example: If the device is On, turns it Off.

--- merossController.py
+++ merossController.py
@@ -1,57 +1,53 @@
 import asyncio
 import os
 import sys
-import tokens as tkn
+
 from meross_iot.http_api import MerossHttpClient
 from meross_iot.manager import MerossManager
 
-EMAIL = tkn.EMAIL
-PASSWORD = tkn.PASSWORD
+EMAIL = "youremail@gmail.com"
+PASSWORD = "yourPassword"
 
-if(len(sys.argv)>1):
+async def main():
+    # Setup the HTTP client API from user-password
+    http_api_client = await MerossHttpClient.async_from_user_password(email=EMAIL, password=PASSWORD)
 
-    async def main():
-        # Setup the HTTP client API from user-password
-        http_api_client = await MerossHttpClient.async_from_user_password(email=EMAIL, password=PASSWORD)
+    # Setup and start the device manager
+    manager = MerossManager(http_client=http_api_client)
+    await manager.async_init()
 
-        # Setup and start the device manager
-        manager = MerossManager(http_client=http_api_client)
-        await manager.async_init()
+    # Retrieve all the MSS310 devices that are registered on this account
+    await manager.async_device_discovery()
+    plugs = manager.find_devices(device_type="mss310")
 
-        # Retrieve all the MSS310 devices that are registered on this account
-        await manager.async_device_discovery()
-        plugs = manager.find_devices(device_type="mss310")
+    if len(plugs) < 1:
+        print("No MSS310 plugs found...")
+    else:
+        # Turn it on channel 0
+        # Note that channel argument is optional for MSS310 as they only have one channel
+        dev = plugs[0]
 
-        if len(plugs) < 1:
-            print("No MSS310 plugs found...")
+        # The first time we play with a device, we must update its status
+        await dev.async_update()
+
+        # Check the current status
+        if dev.is_on():
+            print(f"Turning off {dev.name}...")
+            await dev.async_turn_off(channel=0)
         else:
-            # Turn it on channel 0
-            # Note that channel argument is optional for MSS310 as they only have one channel
-            dev = plugs[0]
+            print(f"Turning on {dev.name}...")
+            await dev.async_turn_on(channel=0)
 
-            # The first time we play with a device, we must update its status
-            #await dev.async_update()
-
-            # We can now start playing with that
-            if(sys.argv[1]=="on"):
-                print(f"Turning on {dev.name}...")
-                await dev.async_turn_on(channel=0)
-            if(sys.argv[1]=="off"):
-                print(f"Turing off {dev.name}")
-                await dev.async_turn_off(channel=0)
-
-        # Close the manager and logout from http_api
-        manager.close()
-        await http_api_client.async_logout()
+    # Close the manager and logout from http_api
+    manager.close()
+    await http_api_client.async_logout()
 
 
-    if __name__ == '__main__':
-        # Windows and python 3.8 requires to set up a specific event_loop_policy.
-        #  On Linux and MacOSX this is not necessary.
-        if os.name == 'nt':
-            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
-        loop.close()
-else:
-    print("You must pass the script some arguments(on/off)")
+if __name__ == '__main__':
+    # Windows and python 3.8 require setting up a specific event_loop_policy.
+    # On Linux and MacOSX this is not necessary.
+    if os.name == 'nt':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
